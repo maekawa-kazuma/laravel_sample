8862a95d7b26f82f000cfa3afdf438a8a8c931fc
maekawa <kazumaekawa7218@gmail.com>
2020-11-26 12:18:10 +0900

    ルーティングの編集(投稿フォーム用)

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,22 @@ Route::get('/', function () {
     $links = \App\Link::all();
     return view('welcome', ['links' => $links]);
 });
+
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+use Illuminate\Http\Request;
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required | max:255',
+        'url' => 'required | url | max:255',
+        'description' => 'required | max:255',
+    ]);
+
+    $link = new App\Link($data);
+    $link->save();
+
+    return redirect('/');
+});

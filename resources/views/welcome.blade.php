fab5285d1c79865e866b713eb13b07a8ce9678c8
maekawa <kazumaekawa7218@gmail.com>
2020-11-26 21:07:36 +0900

    トップページのビュー修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,19 +65,17 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            <!-- @if (Route::has('login')) -->
                 <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
+                    <!-- authは認証 -->
+                    <!-- @auth -->
+                        <a href="{{ url('/home') }}">Homeee</a>
+                    <!-- @else -->
                         <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                        <a href="{{ route('register') }}">Register</a>
+                    <!-- @endauth -->
                 </div>
-            @endif
+            <!-- @endif -->
 
             <div class="content">
                 <div class="title m-b-md">
